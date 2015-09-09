51808c74e8d7bd3d61102b739867264e54247a4f
Todd Hayes <todd@i.tv>
2015-09-09 15:09:40 -0600

    Working chatroom

--- js/parseService.js
+++ js/parseService.js
@@ -6,7 +6,6 @@ app.service('parseService', function($http){
   //On the lines below create a getData method. This method will retrieve data from the parse backend.
   //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
- 
 
 
   //On the line below create the postData method. This method will add data to the parse backend.
@@ -16,7 +15,21 @@ app.service('parseService', function($http){
   //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
   
   //postData method here
-  
+  this.addData = function(message) {
+    return $http({
+      method: "POST",
+      url: 'https://api.parse.com/1/classes/chat',
+      data: {text: message}
+    })
+  };
   
   //getData method here
+   this.getData = function() {
+   return $http({
+     method: 'GET',
+     url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+   })
+ }
+  
+  
 });
